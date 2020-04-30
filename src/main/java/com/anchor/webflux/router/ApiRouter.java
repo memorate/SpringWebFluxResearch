68fbbe320c469f9da5afd77afc48b067eed097d5
mawen <mawen@meicloud.com>
2020-04-30 15:30:41 +0800

    run demo successfully

---
+++ src/main/java/com/anchor/webflux/router/ApiRouter.java
@@ -0,0 +1,27 @@
+package com.anchor.webflux.router;
+
+import com.anchor.webflux.handler.HelloWorldHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class ApiRouter {
+
+    @Resource
+    private HelloWorldHandler helloWorldHandler;
+
+    @Bean("anchor")
+    public RouterFunction<ServerResponse> helloWorld() {
+        return RouterFunctions
+                .route(RequestPredicates.GET("/hello")
+                                .and(RequestPredicates.accept(MediaType.TEXT_PLAIN))
+                        , helloWorldHandler::helloWorld);
+    }
+}
