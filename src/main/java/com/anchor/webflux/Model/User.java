49140df997040818cd3221052e09bfab06c7084c
mawen <mawen@meicloud.com>
2020-05-18 13:46:26 +0800

    增加model

---
+++ src/main/java/com/anchor/webflux/Model/User.java
@@ -0,0 +1,11 @@
+package com.anchor.webflux.Model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
+@NoArgsConstructor
+public class User {
+}
